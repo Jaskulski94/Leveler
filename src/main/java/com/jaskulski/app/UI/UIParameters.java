a403bea2c93c411b88499426513159d9be58069e
Jakub Jaskulski <jakjaskulski@gmail.com>
2019-02-20 22:24:59 +0100

    Added UI class containing static parameters for the project

---
+++ src/main/java/com/jaskulski/app/UI/UIParameters.java
@@ -0,0 +1,22 @@
+package com.jaskulski.app.UI;
+
+import java.awt.*;
+
+public class UIParameters {
+    public static Font fontBig;
+    public static Font fontMedium;
+    public static Font fontSmall;
+    public static Font fontTiny;
+
+    public static Dimension screenDim; // = Toolkit.getDefaultToolkit().getScreenSize();
+    public static int frameWidth; // = Scheme.SCREEN_DIM.width/5;
+    public static int frameHeight; // = Scheme.SCREEN_DIM.height*2/5;
+
+    public static void setScreenDim(){
+
+    }
+
+    public static void setFrameSize(){
+
+    }
+}

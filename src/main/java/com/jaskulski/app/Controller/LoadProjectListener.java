a4e72f0aeaf5ee142cc91f0b61cf73b1c915692b
Jakub Jaskulski <jakjaskulski@gmail.com>
2019-02-21 22:41:03 +0100

    Added empty Listeners for initializing work

---
+++ src/main/java/com/jaskulski/app/Controller/LoadProjectListener.java
@@ -0,0 +1,11 @@
+package com.jaskulski.app.Controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoadProjectListener implements ActionListener {
+
+    public void actionPerformed(ActionEvent arg0){
+
+    }
+}
---
+++ src/main/java/com/jaskulski/app/Controller/NewProjectListener.java
@@ -0,0 +1,11 @@
+package com.jaskulski.app.Controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class NewProjectListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent arg0){
+
+        }
+}

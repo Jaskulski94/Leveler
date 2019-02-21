96ccf55417936d1c85e8229d2d7009fd0efdc338
Jakub Jaskulski <jakjaskulski@gmail.com>
2019-02-21 22:52:30 +0100

    LoadProjectListener now has templets of methonds for loading StartingConditions and TerrainMatrix

--- src/main/java/com/jaskulski/app/Controller/LoadProjectListener.java
+++ src/main/java/com/jaskulski/app/Controller/LoadProjectListener.java
@@ -6,6 +6,19 @@ import java.awt.event.ActionListener;
 public class LoadProjectListener implements ActionListener {
 
     public void actionPerformed(ActionEvent arg0){
+        loadDataFromSelected();
+    }
+
+    public void loadDataFromSelected(){
+        loadStartingConditions();
+        loadTerrainMatrix();
+    }
+
+    public void loadStartingConditions(){
+
+    }
+
+    public void loadTerrainMatrix(){
 
     }
 }
--- src/main/java/com/jaskulski/app/UI/ProjectStarter.java
+++ src/main/java/com/jaskulski/app/UI/ProjectStarter.java
@@ -1,5 +1,8 @@
 package com.jaskulski.app.UI;
 
+import com.jaskulski.app.Controller.LoadProjectListener;
+import com.jaskulski.app.Controller.NewProjectListener;
+
 import javax.swing.*;
 
 public class ProjectStarter extends JPanel {
@@ -10,9 +13,11 @@ public class ProjectStarter extends JPanel {
         this.add(lblTitle);
 
         JButton btnLoadProject = new JButton("Otwórz istniejący projekt");
+        btnLoadProject.addActionListener(new LoadProjectListener());
         this.add(btnLoadProject);
 
         JButton btnNewProject = new JButton("Otwórz istniejący projekt");
+        btnNewProject.addActionListener(new NewProjectListener());
         this.add(btnNewProject);
 
         this.setVisible(true);

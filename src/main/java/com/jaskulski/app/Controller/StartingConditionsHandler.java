5b0d28b69de7e211628fb2ffe9515272722c44ed
Jakub Jaskulski <jakjaskulski@gmail.com>
2019-02-25 15:42:24 +0100

    Added empty handler of StartingConditions

---
+++ src/main/java/com/jaskulski/app/Controller/StartingConditionsHandler.java
@@ -0,0 +1,13 @@
+package com.jaskulski.app.Controller;
+
+import com.jaskulski.app.Data.StartingConditions;
+
+public class StartingConditionsHandler {
+    StartingConditions startingConditions;
+
+    public StartingConditionsHandler(StartingConditions startingConditions1){
+        this.startingConditions = startingConditions1;
+    }
+
+
+}

81d404902efcd1446d9a527f6d64002e4f922ad6
Jakub Jaskulski <jakjaskulski@gmail.com>
2019-02-28 14:00:00 +0100

    Changed SqauresGrids layout to GridBagLayout

--- src/main/java/com/jaskulski/app/UI/SquareGridPanel.java
+++ src/main/java/com/jaskulski/app/UI/SquareGridPanel.java
@@ -12,19 +12,28 @@ public class SquareGridPanel extends JPanel {
         this.sizeX = size1;
         this.sizeY = size2;
 
-        this.setLayout(new GridLayout(sizeY, sizeY));
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        GridBagConstraints GBC = new GridBagConstraints();
+        this.setLayout(gridBagLayout);
+        GBC.gridx = 0;
+        GBC.gridy = 0;
+        GBC.fill = GridBagConstraints.BOTH;
+        GBC.weightx = 1;
+        GBC.weighty = 1;
 
         txtSquares = new JTextField[sizeX][sizeY];
         lblIndex = new JLabel[sizeX][sizeY];
 
         for(int i = 0; i<sizeX; i++){
             for(int j = 0; j<sizeY; j++){
+                GBC.gridx = i;
+                GBC.gridy = j;
                 txtSquares[i][j] = new JTextField();
-                this.add(txtSquares[i][j]);
+                this.add(txtSquares[i][j], GBC);
 
                 String index = (i+1) + "-" + (j+1);
                 lblIndex[i][j] = new JLabel(index);
-                this.add(lblIndex[i][j]);
+                this.add(lblIndex[i][j], GBC);
             }
         }
         UIParameters.setFontToAll(this, UIParameters.fontMedium);

fd67056c3bc9dd0149456590e96bbb2abeb29b20
Jakub Jaskulski <jakjaskulski@gmail.com>
2019-03-03 14:00:00 +0100

    Working on ShowSC Listener and Frame

--- src/main/java/com/jaskulski/app/Controller/ShowSCListener.java
+++ src/main/java/com/jaskulski/app/Controller/ShowSCListener.java
@@ -1,12 +1,20 @@
 package com.jaskulski.app.Controller;
 
+import com.jaskulski.app.Data.StartingConditions;
+import com.jaskulski.app.UI.ShowSCFrame;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class ShowSCListener implements ActionListener {
 
+    private StartingConditions startingConditions;
 
+    public ShowSCListener (){
+        StartingConditionsHandler startingConditionsHandler = new StartingConditionsHandler();
+
+    }
     public void actionPerformed(ActionEvent actionEvent) {
-
+        ShowSCFrame showSCFrame = new ShowSCFrame(startingConditions);
     }
 }
--- src/main/java/com/jaskulski/app/UI/ShowSCFrame.java
+++ src/main/java/com/jaskulski/app/UI/ShowSCFrame.java
@@ -1,19 +1,29 @@
 package com.jaskulski.app.UI;
 
+import com.jaskulski.app.Data.StartingConditions;
+
 import javax.swing.*;
 
 public class ShowSCFrame extends JFrame {
+    StartingConditions startingConditions;
 
-    public ShowSCFrame(){
+    public ShowSCFrame(StartingConditions startingConditions1){
+        this.startingConditions = startingConditions1;
+
         this.setSize(UIParameters.dimFrame);
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-
+        CheckProjectInitiatorPanel CheckPIPanel = new CheckProjectInitiatorPanel();
     }
 
     public class CheckProjectInitiatorPanel extends NewProjectInitiatorPanel{
 
 
+        public void completeFields(){
+
+        }
     }
+
+
 }

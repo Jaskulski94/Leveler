079afb50549cdeb51dbbacedb9b84f1996587c40
Jakub Jaskulski <jakjaskulski@gmail.com>
2019-03-13 18:54:55 +0100

    Added checking direction of Field Lean, working of finding Zero Points

--- src/main/java/com/jaskulski/app/Controller/ProjectCalculator.java
+++ src/main/java/com/jaskulski/app/Controller/ProjectCalculator.java
@@ -18,7 +18,7 @@ public class ProjectCalculator {
         squaresY = sizeY - 1;
     }
 
-    public void calculationOfOrdinatesOfBalanceSheet(){
+    public void calculateOrdinatesOfBalanceSheet(){
 
     }
 
@@ -57,6 +57,8 @@ public class ProjectCalculator {
     }
 
     public void setSGProjectOrdinates(){
+        checkLeanDirection();
+
         double calculatedOrdiante = ordinateOfBalanceSheet();
         double calculatedLean = calculatedOrdiante*startingConditions.getFieldLean()/2/100;
         double calculatedDif;
@@ -67,6 +69,50 @@ public class ProjectCalculator {
                 squareGrid.squares[i][j].setProjectOrdinate(calculatedOrdiante);
                 calculatedDif = squareGrid.squares[i][j].terrainOrdinate - calculatedOrdiante;
                 squareGrid.squares[i][j].setOrdinateDifference(calculatedDif);
+            }
+        }
+    }
+
+    private void checkLeanDirection() {
+        double LeanUp = 0;
+        double LeanDown = 0;
+
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = 0; j < (sizeY/2); j++) {
+                LeanUp += squareGrid.squares[i][j].getTerrainOrdinate();
+            }
+        }
+
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = (sizeY/2); j < sizeY; j++) {
+                LeanDown += squareGrid.squares[i][j].getTerrainOrdinate();
+            }
+        }
+        double newLean = Math.abs(startingConditions.getFieldLean());
+        if (LeanUp > LeanDown){
+            startingConditions.setFieldLean(newLean);
+        } else {
+            startingConditions.setFieldLean((-1)*newLean);
+        }
+    }
+
+    public void findZeroPoints(){
+        double firstDif;
+        double secondDif;
+        double calculatedZero;
+        double copiedZero;
+        double hOfZero;
+
+        for(int i = 0; i<sizeX-1; i++) {
+            for (int j = 0; j < sizeY-1; j++) {
+                firstDif = squareGrid.squares[i][j].getOrdinateDifference();
+                secondDif = squareGrid.squares[++i][j].getOrdinateDifference();
+                if((firstDif*secondDif)<0){
+                    calculatedZero = (firstDif * startingConditions.getSquareSide())/(firstDif * secondDif);
+                    copiedZero = j * startingConditions.getSquareSide();
+                    hOfZero = ordinateOfBalanceSheet()+startingConditions.getFieldLean()*calculatedZero;
+                    squareGrid.ListOfZeroPoints.add(squareGrid.getNewZeroPoints(calculatedZero, copiedZero, hOfZero));
+                }
             }
         }
     }
--- src/main/java/com/jaskulski/app/Data/SquareGrid.java
+++ src/main/java/com/jaskulski/app/Data/SquareGrid.java
@@ -2,6 +2,7 @@ package com.jaskulski.app.Data;
 
 import java.awt.*;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 import lombok.Data;
 
@@ -9,6 +10,7 @@ import lombok.Data;
 public class SquareGrid implements Serializable {
 
     public Square[][] squares;
+    public ArrayList<ZeroPoints> ListOfZeroPoints = new ArrayList<>();
 
     public SquareGrid(int sizeX, int sizeY) {
         squares = new Square[sizeX][sizeY];
@@ -36,5 +38,22 @@ public class SquareGrid implements Serializable {
             this.index = index1;
             this.terrainOrdinate = value1;
         }
+    }
+
+    @Data
+    public class ZeroPoints implements Serializable{
+        public double xOrdinate;
+        public double yOrdinate;
+        public double hOrdinate;
+
+        public ZeroPoints (double x1, double y1, double h1){
+            xOrdinate = x1;
+            yOrdinate = y1;
+            hOrdinate = h1;
+        }
+    }
+
+    public ZeroPoints getNewZeroPoints(double x1, double y1, double h1){
+        return new ZeroPoints(x1, y1, h1);
     }
 }

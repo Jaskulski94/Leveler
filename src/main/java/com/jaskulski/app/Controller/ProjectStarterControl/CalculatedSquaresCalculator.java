a50aa5f0e842c6a55e78a60c9966651a497df3f7
Jakub Jaskulski <jakjaskulski@gmail.com>
2019-03-15 16:48:37 +0100

    Working on CalculatedSquaresCalculator

---
+++ src/main/java/com/jaskulski/app/Controller/ProjectStarterControl/CalculatedSquaresCalculator.java
@@ -0,0 +1,19 @@
+package com.jaskulski.app.Controller.ProjectStarterControl;
+
+import com.jaskulski.app.Data.CalculatedSquares;
+import com.jaskulski.app.Data.SquareGrid;
+import com.jaskulski.app.Data.StartingConditions;
+
+public class CalculatedSquaresCalculator {
+    private SquareGrid squareGrid;
+    private StartingConditions startingConditions;
+    private CalculatedSquares calculatedSquares;
+
+    public CalculatedSquaresCalculator(){
+
+    }
+
+    public void setSquaresCorners(){
+
+    }
+}
